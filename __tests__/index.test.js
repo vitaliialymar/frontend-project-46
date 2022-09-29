6ca1e6250f4e242789a28c1736e23656a17cade0
Vitaliia Lymar <vitaliialymar@gmail.com>
2022-09-29 19:41:01 +0300

    step 4

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,18 +1,25 @@
 #!/usr/bin/env node
 import { fileURLToPath } from 'url';
 import path from 'path';
-import fs from 'fs';
 import genDiff from '../src/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
+
+const file1 = getFixturePath('file1.json');
+const file2 = getFixturePath('file2.json');
+
+const expected = `{
+    - follow: false
+      host: hexlet.io
+    - proxy: 123.234.53.22
+    - timeout: 50
+    + timeout: 20
+    + verbose: true
+    }`;
 
 test('test one', () => {
-  const actual = genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'));
-  const result = readFile('result.txt');
-  const actualresult = result.trim();
-  expect(actual).toEqual(actualresult);
+  expect(genDiff(file1, file2)).toEqual(expected);
 });
